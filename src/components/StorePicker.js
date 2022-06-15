3208d4a6deb00be22c0517b296ad2322e1e3daed
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-06-14 20:16:20 -0400

    Adding JSX to Component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
-    render(){
-        return <p>Store Picker</p>
+    render() {
+        return (
+            //same as <React.Fragment> 
+            <>
+                <p>Fish!</p>
+                <form className="store-selector">
+                    <h2>Please Enter a Store</h2>
+                    <input type="text" required placeholder="Store Name"/>
+                    <button type="submit">Visit Store →</button>
+                </form>
+            </>
+        )
     }
 }
 
