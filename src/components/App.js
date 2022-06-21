b18a5e648cb3fea55a14e13b19c174200e63a64a
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-06-20 20:27:52 -0400

    Adding data to state

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,25 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = (fish) => {
+        //take a copy of state
+        const fishes = {...this.state.fishes};
+
+        //add new fish
+        fishes[`fish${Date.now()}`] = fish;
+
+        //set new fishes object ot state
+        this.setState({
+            fishes: fishes
+        }); 
+
+    };
+
     render(){
         return (
             <div className="catch-of-the-day">
@@ -11,7 +30,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market"/>
                 </div> 
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish}/>
             </div>
         )
     }
