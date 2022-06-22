110371ddc413e94bd27d9446d39be0f0e837f3fe
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-06-21 22:15:44 -0400

    Adding Order total to page

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
     render(){
+        const orderIds = Object.keys(this.props.order);
+        const total = orderIds.reduce((prevTotal, key) => {
+            const fish = this.props.fishes[key];
+            const count = this.props.order[key];
+
+            //making sure that fish is not sold while adding to cart    
+            const isAvailable = fish && fish.status === 'available';
+
+            if(isAvailable){
+                return prevTotal + (count * fish.price);
+            }
+            //if it is not available, skip it   
+            return prevTotal;
+            //start at zero
+        }, 0);
+
         return (
-            <div className="order">Order</div>
+            <div className="order-wrap">
+                <h2>Order</h2> 
+                {orderIds} 
+                <div className="total">
+                    <strong>{formatPrice(total)}</strong>
+                </div>
+            </div>
         );
     }
 }
